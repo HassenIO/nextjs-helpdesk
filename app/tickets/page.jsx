b561dfed4a8b4e517d5051aecfe271e2436ed47f
Hassen Taidirt <hi@hassen.io>
2023-08-11 01:29:01 +0200

    Update Tickets page to include top nav bar

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -3,7 +3,12 @@ import TicketList from "./TicketList";
 export default function TicketsPage() {
   return (
     <main>
-      <h2>Tickets page</h2>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p><small>Open tickets:</small></p>
+        </div>
+      </nav>
       <TicketList/>
     </main>
   )
