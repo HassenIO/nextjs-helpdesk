80a08d321dfe40db6edcc90b861e44ede608edc7
Hassen Taidirt <hi@hassen.io>
2023-08-15 19:57:41 +0200

    Refactor creating route groups

---
+++ app/(auth)/login/page.jsx
@@ -0,0 +1,7 @@
+export default function LogionPage() {
+  return (
+    <main>
+      <h2 className="text-center">Login</h2>
+    </main>
+  )
+}
---
+++ app/(auth)/signup/page.jsx
@@ -0,0 +1,7 @@
+export default function SignupPage() {
+  return (
+    <main>
+      <h2 className="text-center">Signup</h2>
+    </main>
+  )
+}
