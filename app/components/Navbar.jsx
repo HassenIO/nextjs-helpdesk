57200276102bfe354403a379b40a488e4ae5a066
Hassen Taidirt <hi@hassen.io>
2023-09-03 00:59:12 +0200

    Add a "Create Ticket" button in the Navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,11 +4,14 @@ import Logo from 'public/dojo-logo.png'
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="flex">
       <Image src={Logo} alt="Dojo Helpdesk Logo" width={70} quality={100} placeholder="empty" />
       <h1>Dojo Helpdesk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
+      <Link href="/tickets/create" className="ml-auto">
+        <button className="btn-primary">Create a Ticket</button>
+      </Link>
     </nav>
   )
 }
