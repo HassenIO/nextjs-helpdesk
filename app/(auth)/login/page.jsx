1eeba08a2b2c33f3a809f2f1d9a05fbc6366092a
Hassen Taidirt <hi@hassen.io>
2023-09-03 18:18:25 +0200

    Create login logic

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,21 +1,39 @@
 'use client'
 
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import { useState } from "react"
+
+// Components
 import AuthForm from "../AuthForm"
+import { useRouter } from "next/navigation"
 
 // export const metadata = {
 //   title: 'Dojo Helpdesk - Access your account',
 // } // Cannot be used with 'use client'
 
 export default function LoginPage() {
+  const [error, setError] = useState(null)
+  const router = useRouter()
+
   const handleSubmit = async (e, email, password) => {
     e.preventDefault()
-    console.log('Login user with', email, password)
+    setError(null)
+    
+    const supabase = createClientComponentClient()
+    const { error } = await supabase.auth.signInWithPassword({ email, password })
+
+    if (error) {
+      setError(error.message)
+    } else {
+      router.push('/')
+    }
   }
 
   return (
     <main>
       <h2 className="text-center">Login</h2>
       <AuthForm handleSubmit={handleSubmit} />
+      {error && <div className="error">{error}</div>}
     </main>
   )
 }
