c35d0db6881b963ec717519596a22b03b3476a6f
Hassen Taidirt <hi@hassen.io>
2023-09-03 18:31:37 +0200

    Protect auth pages if user already authenticated

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,12 +1,26 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+// Components
 import Link from 'next/link'
 
-export default function AuthLayout({ children }) {
+export default async function AuthLayout({ children }) {
+  const supabase = createServerComponentClient({ cookies })
+  const { data } = await supabase.auth.getSession()
+
+  if (data.session) {
+    redirect('/')
+  }
+
   return (
     <>
       <nav>
         <h1>Dojo Helpdesk</h1>
-        <Link href="/signup">Signup</Link>
-        <Link href="/login">Login</Link>
+        <div className="ml-auto">
+          <Link href="/signup" className="mx-4">Signup</Link>
+          <Link href="/login" className="mx-4">Login</Link>
+        </div>
       </nav>
       {children}
     </>
